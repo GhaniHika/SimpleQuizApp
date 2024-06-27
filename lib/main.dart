391092bc58cc2d11d53a615168ec6792de91a56c
Hika <ghanirasyidhika@gmail.com>
2024-06-27 17:18:57 +0800

    make question data and answer data and try to show it

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
-import 'package:adv_basics/gradient_background.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: GradientBackground(Colors.lightBlue, Colors.white),
-      ),
-    )
-  );
+  runApp(const Quiz());
 }
---
+++ lib/questions_screen.dart
@@ -0,0 +1,35 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            currentQuestion.text,
+          ),
+          const SizedBox(height: 30,),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(answerText: item, onTap: () {});
+          }),
+        ],
+      ),
+    );
+  }
+}
